dfad5862c340211d6315ad6c93590c4e61cca0bb
Salvc <scamarote20@scienceleadership.org>
2019-09-25 09:21:03 -0400

    I introduced Cats and a new Dog!

---
+++ src/org/sla/Cat.java
@@ -0,0 +1,19 @@
+package org.sla;
+
+// Author: Mr. Hernandez
+
+public class Cat {
+    // Fields describe the class
+    String color;
+    String size;
+    int age;
+    String name;
+    String petType;
+    String mood;
+
+    void meows() {
+        System.out.println("Meow!");
+    }
+
+
+}
--- src/org/sla/Pets.java
+++ src/org/sla/Pets.java
@@ -7,7 +7,21 @@ public class Pets {
 
     // Method
     public static void main(String[] args) {
-        Dog[] dogs = new Dog[2];
+        Cat[] cats = new Cat[1];
+        cats[0] = new Cat();
+        cats[0].age = 4;
+        cats[0].color = "Black & White";
+        cats[0].size = "Small";
+        cats[0].name = "Lola";
+        cats[0].petType = "Cat";
+        cats[0].mood = "Sassy";
+        cats[0].meows();
+
+        for (int i = 0; i < cats.length; i++) {
+            System.out.println(cats[i].name + " is a " + cats[i].petType + " of age " + cats[i].age + " and is color " + cats[i].color + " and is of size " + cats[i].size + " and is acting " + cats[i].mood);
+        }
+
+        Dog[] dogs = new Dog[3];
         // Mr. Hernandez did this
         dogs[0] = new Dog();
         dogs[0].age = 2;
@@ -28,6 +42,13 @@ public class Pets {
         dogs[1].size = "Small";
         dogs[1].name = "Java";
         dogs[1].petType = "dog";
+
+        dogs[2] = new Dog();
+        dogs[2].age = 6;
+        dogs[2].color = "Black";
+        dogs[2].size = "Small";
+        dogs[2].name = "Gianny";
+        dogs[2].petType = "dog";
 
         for (int i = 0; i < dogs.length; i++) {
             System.out.println(dogs[i].name + " is a " + dogs[i].petType + " of age " + dogs[i].age + " and is color " + dogs[i].color + " and is of size " + dogs[i].size);
