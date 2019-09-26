48011d0b4cc864d5d251e4f5f0c9949688cea3c5
W1derfire <niccobeanz@gmail.com>
2019-09-26 09:41:15 -0400

    Added Birds

---
+++ src/org/sla/Bird.java
@@ -0,0 +1,65 @@
+package org.sla;
+
+//This is Nicco's stuff.
+
+
+public class Bird {
+
+    private String color;
+    private String breed;
+    private String name;
+    private int age;
+
+    public Bird(String color, String breed, String name, int age){
+
+        this.color = color;
+        this.breed = breed;
+        this.name = name;
+        this.age = age;
+
+    }
+
+    String getColor() {
+        return this.color;
+    }
+
+    String getBreed() {
+        return this.breed;
+    }
+
+    String getName() {
+        return this.name;
+    }
+
+    int getAge() {
+        return this.age;
+    }
+
+    void addToArray(Bird[] birds, int n){
+
+        birds[n] = this;
+
+    }
+
+    void chirp(){
+
+        System.out.println("Chirp!");
+
+    }
+
+    boolean fly(){
+
+        if(this.breed.equals("Ostrich") || this.breed.equals("Penguin") || this.breed.equals("Roadrunner")) {
+
+            System.out.println(this.name + " can't fly!");
+            return false;
+
+        }
+
+        System.out.println("Swoosh");
+        return true;
+
+    }
+
+
+}
--- src/org/sla/Pets.java
+++ src/org/sla/Pets.java
@@ -7,7 +7,9 @@ public class Pets {
 
     // Method
     public static void main(String[] args) {
-        Dog[] dogs = new Dog[2];
+        Dog[] dogs = new Dog[3];
+        Bird[] birds = new Bird[2];
+
         // Mr. Hernandez did this
         dogs[0] = new Dog();
         dogs[0].age = 2;
@@ -24,13 +26,37 @@ public class Pets {
 
         dogs[1] = new Dog();
         dogs[1].age = 2;
-        dogs[1].color = "White";
+        dogs[1].color = "Brown";
         dogs[1].size = "Small";
         dogs[1].name = "Java";
         dogs[1].petType = "dog";
 
+        // Pretty much everything from here on out is from Nicco.
+
+        dogs[2] = new Dog();
+        dogs[2].age = 2;
+        dogs[2].color = "Gray";
+        dogs[2].size = "Medium";
+        dogs[2].name = "Processing";
+        dogs[2].petType = "dog";
+
+
+
         for (int i = 0; i < dogs.length; i++) {
             System.out.println(dogs[i].name + " is a " + dogs[i].petType + " of age " + dogs[i].age + " and is color " + dogs[i].color + " and is of size " + dogs[i].size);
         }
+
+        Bird Red = new Bird("Red", "Angry", "Red",5);
+        Bird Chuck = new Bird("Yellow", "Triangle","Chuck",5);
+
+        Red.addToArray(birds,0);
+        Chuck.addToArray(birds,1);
+
+        System.out.println("\n");
+
+        for(int i = 0; i < birds.length; i ++){
+            System.out.println(birds[i].getName() + " is a " + birds[i].getBreed() + " bird of color " + birds[i].getColor() + " and age " + birds[i].getAge() + ".");
+        }
+
     }
 }
