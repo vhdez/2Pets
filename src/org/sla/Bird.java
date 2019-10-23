37755bb4401e0ba0e255e4a9b817a96f655f1b7e
W1derfire <niccobeanz@gmail.com>
2019-10-23 08:39:27 -0400

    Got rid of a lot of stuff to prepare for incoming masterclass

--- src/org/sla/Bird.java
+++ src/org/sla/Bird.java
@@ -1,14 +1,18 @@
 package org.sla;
+import java.util.Scanner;
 
 //This is Nicco's stuff.
 
 
 public class Bird {
 
+    //private Scanner input;
+
     private String color;
     private String breed;
     private String name;
     private int age;
+    private String food;
 
     public Bird(String color, String breed, String name, int age){
 
@@ -35,11 +39,13 @@ public class Bird {
         return this.age;
     }
 
+
     void addToArray(Bird[] birds, int n){
 
         birds[n] = this;
 
     }
+
 
     void chirp(){
 
@@ -53,17 +59,44 @@ public class Bird {
 
     boolean fly(){
 
-        if(this.breed.equals("Ostrich") || this.breed.equals("Penguin") || this.breed.equals("Roadrunner")) {
+        if(this.breed.equals("Ostrich") || this.breed.equals("Penguin") || this.breed.equals("Roadrunner") || this.breed.equals("Peacock")) {
 
             System.out.println(this.name + " can't fly!");
             return false;
 
         }
 
-        System.out.println("Swoosh");
+        System.out.println("Swoosh!");
         return true;
 
     }
+
+
+    boolean feed(Scanner input){
+
+        System.out.println("Time to feed " + this.name + "!\nBut with what?");
+
+        this.food = input.nextLine();
+        this.food = this.food.toLowerCase();
+
+        System.out.println("\n");
+
+        if(this.food.equals("cracker")){
+            System.out.println(this.name + " is uninspired by your \"creative\" food options.");
+            return true;
+        }else if(this.food.equals("tuna")){
+            System.out.println("What were you thinking?! You summoned the mighty eagle.");
+            return false;
+        }else if(this.food.equals("egg") || this.food.equals("bird") || this.food.equals("birds")){
+            System.out.println("What the heck is wrong with you?!");
+            return false;
+        }else{
+            System.out.println(this.name + " is happy!");
+            return true;
+        }
+
+    }
+
 
 
 }
--- src/org/sla/Pets.java
+++ src/org/sla/Pets.java
@@ -1,4 +1,6 @@
 package org.sla;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 // Author: Mr. Hernandez
 
@@ -10,43 +12,9 @@ public class Pets {
         Dog[] dogs = new Dog[256];
         Bird[] birds = new Bird[2];
 
-        // Mr. Hernandez did this
-        dogs[0] = new Dog();
-        dogs[0].age = 2;
-        dogs[0].color = "Black";
-        dogs[0].size = "Big";
-        dogs[0].name = "Lincoln";
-        dogs[0].petType = "dog";
-
-        dogs[0].bark();
-        String someone = "Nicco";
-        if (dogs[0].bites(someone)) {
-            System.out.println("bit someone!");
-        }
-
-        dogs[1] = new Dog();
-        dogs[1].age = 2;
-        dogs[1].color = "Brown";
-        dogs[1].size = "Small";
-        dogs[1].name = "Java";
-        dogs[1].petType = "dog";
-
-        // Pretty much everything from here on out is from Nicco.
-
-        dogs[2] = new Dog();
-        dogs[2].age = 2;
-        dogs[2].color = "Gray";
-        dogs[2].size = "Medium";
-        dogs[2].name = "Processing";
-        dogs[2].petType = "dog";
+        Scanner input = new Scanner(System.in);
 
 
-        for (int i = 0; i < dogs.length; i++) {
-            if (dogs[i] != null) {
-                System.out.println(dogs[i].name + " is a " + dogs[i].petType + " of age " + dogs[i].age + " and is color " + dogs[i].color + " and is of size " + dogs[i].size);
-                System.out.println("-----------------------------------");
-            }
-        }
 
 
         Bird Red = new Bird("Red", "Angry", "Red", 5);
@@ -61,36 +29,10 @@ public class Pets {
             System.out.println(birds[i].getName() + " is a " + birds[i].getBreed() + " bird of color " + birds[i].getColor() + " and age " + birds[i].getAge() + ".");
         }
 
+//<<<<<<< Updated upstream
 
-        Dog dog1 = new Dog("Nicco", 4, "Brown", "Big", "Dog", 6);
-        Dog dog2 = new Dog("Jack", 2, "Black", "Small", "Dog", 9);
-        Dog dog3 = new Dog("Sal", 8, "White", "Tall", "Dog", 4);
-        Dog dog4 = new Dog("Hayden", 3, "Brown", "Fat", "Dog", 2);
-        dogs[3] = dog1;
-        dogs[4] = dog2;
-        dogs[5] = dog3;
-        dogs[6] = dog4;
-        for (int i = 3; i < dogs.length; i++) {
-            if(dogs[i] != null) {
-                dogs[i].announce();
-            }
-        }
-        for (int i = 3; i < dogs.length; i++) {
-            if(dogs[i] != null) {
-                if (i < 2) {
-                    dogs[i].bark();
-                } else {
-                    dogs[i].fetch();
-                }
-            }
-        }
+        Red.feed(input);
 
-
-        System.out.println("-----------------------------------");
-        Parrot parrot = new Parrot("Parrot", 5, "Red", "parrot", true);
-        parrot.addFriend(dog1);
-        parrot.addFriend(dog3);
-        parrot.announce();
-        parrot.announceFriends();
+//>>>>>>> Stashed changes
     }
 }
